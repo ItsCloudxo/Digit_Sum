3a6e86344917aa408d4d4df93bec8727812976d3
ItsCloudxo <ItsCloudxo@DESKTOP-469V821>
2022-03-22 18:49:49 +0100

    Add project files.

---
+++ Digit_Sum/Program.cs
@@ -0,0 +1,18 @@
+﻿int n, sum = 0;
+
+bool isNumber = int.TryParse(Console.ReadLine(), out n);
+n = Math.Abs(n);
+
+if (isNumber)
+{
+    while (n != 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
+else
+{
+    Console.WriteLine("Number wasn't an input");
+}
